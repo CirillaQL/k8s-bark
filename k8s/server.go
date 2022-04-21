8410f5dada033d63259afef1c15d4f0c1dbdb6c8
lu.qian <1194946223@qq.com>
2022-04-21 20:20:46 +0800

    Use List-Watch to watch Pods Events

--- k8s/server.go
+++ k8s/server.go
@@ -9,9 +9,13 @@ import (
 	"path/filepath"
 	"time"
 
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
@@ -59,13 +63,14 @@ func NewK8sWatch(location, barkServer, barkToken string) (k8swatch *K8sWatch) {
 	k8swatch.clientset = clientset
 	bark := bark.NewBark(barkServer, barkToken)
 	k8swatch.bark = bark
+
 	return k8swatch
 }
 
 func (k8swatch *K8sWatch) Watch() {
+	stopper := make(chan struct{})
 	go k8swatch.bark.HealthzCheck()
-	go k8swatch.watchPodsStatus()
-	go k8swatch.watchK8sEvents()
+	go k8swatch.watchPodsStatus(stopper)
 	go k8swatch.bark.Send()
 	for {
 		pods, err := k8swatch.clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
@@ -78,7 +83,6 @@ func (k8swatch *K8sWatch) Watch() {
 		}
 		// k8swatch.Push(m)
 		LOG.Infof("%+v", m)
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
 		time.Sleep(10 * time.Second)
 	}
@@ -89,28 +93,46 @@ func (k8swatch *K8sWatch) Push(message bark.Message) {
 }
 
 // watchPodsStatus 监控Pods Status
-func (k8swatch *K8sWatch) watchPodsStatus() {
-	for {
-		pods, err := k8swatch.clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		for _, pod := range pods.Items {
-			if pod.Status.Phase != "Running" {
-				LOG.Infof("Pod Name: %s, Pod Status: %s", pod.Name, pod.Status.Phase)
-			}
-		}
-		time.Sleep(5 * time.Second)
-	}
-}
+func (k8swatch *K8sWatch) watchPodsStatus(stopper chan struct{}) {
+	// 初始化informer
+	podFactory := informers.NewSharedInformerFactory(k8swatch.clientset, 0)
+	podInformer := podFactory.Core().V1().Pods().Informer()
+	go podFactory.Start(stopper)
 
-// watchK8sEvents 监控K8s Events
-func (k8swatch *K8sWatch) watchK8sEvents() {
-	for {
-		events, _ := k8swatch.clientset.CoreV1().Events("").List(context.TODO(), metav1.ListOptions{})
-		for _, item := range events.Items {
-			fmt.Println(item.Type)
-		}
-		time.Sleep(5 * time.Second)
+	// 从apiserver 同步资源，即List
+	if !cache.WaitForCacheSync(stopper, podInformer.HasSynced) {
+		LOG.Error("Timed out waiting for caches to sync")
+		return
 	}
+
+	// 使用自定义Handler
+	podInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			pod := obj.(*v1.Pod)
+			m := bark.Message{
+				Status:      "Pods",
+				Information: fmt.Sprintf("Pod_%s_is_created", pod.Name),
+			}
+			k8swatch.Push(m)
+			LOG.Infof("%+v", m)
+		},
+		UpdateFunc: func(old, new interface{}) {
+			pod := new.(*v1.Pod)
+			m := bark.Message{
+				Status:      "Pods",
+				Information: fmt.Sprintf("Pod_%s_is_updated", pod.Name),
+			}
+			k8swatch.Push(m)
+			LOG.Infof("%+v", m)
+		},
+		DeleteFunc: func(obj interface{}) {
+			pod := obj.(*corev1.Pod)
+			m := bark.Message{
+				Status:      "Pods",
+				Information: fmt.Sprintf("Pod_%s_is_deleted", pod.Name),
+			}
+			k8swatch.Push(m)
+			LOG.Infof("%+v", m)
+		},
+	})
 }
